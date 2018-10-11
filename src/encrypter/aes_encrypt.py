71a7e2685ac01436a56ab2a69200fce96d8cba9a
tlind15 <tlind15@gmail.com>
2018-10-11 16:22:37 -0700

    implement AES encryption

--- src/encrypter/aes_encrypt.py
+++ src/encrypter/aes_encrypt.py
@@ -1,3 +1,36 @@
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from cryptography.hazmat.primitives.ciphers import Cipher, modes
+from cryptography.hazmat.backends import default_backend
+from os import urandom
+from cryptography.hazmat.primitives import padding
+from encryptor import Encryptor
+
+
+class AESEncrypt(Encryptor):
+
+    def encrypt(self, message, key=None):
+        if key is None:
+            key = self.get_key()
+
+        algorithm = AES(key)
+        cipher = Cipher(algorithm, mode=modes.CBC(self.get_iv()), backend=default_backend())
+        return cipher.encryptor().update(self.pad_data(message)), key
+
+    def get_key(self):
+        return urandom(32)
+
+    def get_iv(self):
+        return urandom(16)
+
+    def pad_data(self, message):
+        encoded_message = message.encode()
+        if len(encoded_message) % 16 == 0:
+            return encoded_message
+        else:
+            padder = padding.PKCS7(128).padder()
+            padded_data = padder.update(message.encode())
+            padded_data += padder.finalize()
+            return padded_data
 
 
 
