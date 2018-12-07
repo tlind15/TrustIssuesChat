6d587e0a115d00880e9e5048307a79e80187635a
tlind15 <tlind15@gmail.com>
2018-12-07 01:10:47 -0800

    clean up code

--- src/main.py
+++ src/main.py
@@ -2,10 +2,3 @@ from src.ui.trustissues_chat import TrustIssuesChat
 
 TrustIssuesChat.start()
 
-'''controller = EncryptionController(PlaintextMessage(text), "C:\\Users\\tlindblom\\RSAKeys\\public.pem")
-encrypted_message = controller.encrypt_message()
-
-print(encrypted_message.get_text())
-print(encrypted_message.key_ciphertext)
-print(encrypted_message.message_authentication_tag)'''
-
--- src/temp_decrypt/rsa_decrypt.py
+++ src/temp_decrypt/rsa_decrypt.py
@@ -1,7 +1,7 @@
 from src.temp_decrypt.decryptor import Decryptor
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
 
